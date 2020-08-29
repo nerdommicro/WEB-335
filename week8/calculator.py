2123c634db6203e46333e3c0173241c7bda12168
michellenesbitt <mn218@nerdymail.com>
2020-08-29 11:13:08 -0400

    mn

---
+++ week8/calculator.py
@@ -0,0 +1,21 @@
+#============================================
+#Title: calculator.py
+#Author: Richard Krasso
+#Date: 25 Aug 2020
+#Modified By: Michelle Nesbitt
+#Description: Demonstrates Python functions
+# #==============================================
+
+
+def add (num1, num2):
+    return num1 + num2
+
+def subtract (num1, num2):
+    return num1 - num2
+
+def divide (num1, num2):
+    return num1 / num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
